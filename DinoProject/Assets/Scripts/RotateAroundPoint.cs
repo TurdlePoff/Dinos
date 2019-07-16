a16da8efdad3592f0dcee46eb094b0d81b1514b5
Vivian <seikyura@gmail.com>
2019-07-16 16:53:56 +1200

    First Push

---
+++ DinoProject/Assets/Scripts/RotateAroundPoint.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAroundPoint : MonoBehaviour
+{
+    public float m_fTarget = 100.0f;
+    [SerializeField] private Transform m_Target;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(m_Target)
+        {
+            transform.RotateAround(m_Target.position, m_Target.up, m_fTarget * Time.deltaTime);
+        }
+    }
+}
