4b3a6bcfdcbc71b60b72e1f60fa81e8e6f418334
Kerry <k.of.blades@gmail.com>
2019-07-24 16:38:33 +1200

    Item scriptable object

---
+++ DinoProject/Assets/Scripts/Items/Item.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Item : ScriptableObject
+{
+    [Header("Visual Properties")]
+    public Sprite m_2DSprite;
+    public Mesh m_3DModel;
+    public string m_strDescription;
+
+    //public virtual 
+}
