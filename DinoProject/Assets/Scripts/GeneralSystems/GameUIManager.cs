404029217fefefbf02689370da0827660b67c5ac
Vivian <seikyura@gmail.com>
2019-08-07 17:14:38 +1200

    GameUIManager

---
+++ DinoProject/Assets/Scripts/GeneralSystems/GameUIManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameUIManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] GameObject m_rInventory;
+    
+    public void SetInventoryActive(Toggle _toggle)
+    {
+        if (!_toggle)
+            return;
+
+        m_rInventory.SetActive(_toggle.isOn);
+    }
+}
