ff93aff9d4b48e8023c8862d3a1a1f14c5e0e0c5
Fruzum <sallygillbanks@gmail.com>
2019-08-11 11:28:45 +1200

    Created new level (for feeding dinos)

---
+++ DinoProject/Assets/Scripts/Items/DisplayInventoryOnFeed.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayInventoryOnFeed : MonoBehaviour
+{
+    public int m_iInventoryPosition = 0;
+
+    private bool m_bDisplayCrops = true;
+    private int m_iCurrentInventoryOffset = 0;
+    
+
+    public void SetCurrentOffSet(int _iOffSet)
+    {
+        m_iCurrentInventoryOffset = _iOffSet;
+    }
+
+    public Image GetCurrentImageToDisplay()
+    {
+        // Find inventory amount and display at this position
+        int iPosition = m_iInventoryPosition + (m_iCurrentInventoryOffset * 8); //8 inventory slots, so offset needs to be adjusted by 8
+
+        //Find image in invenotyr position iPosition
+
+        //if none, return null
+        return null;
+    }
+}
