92433e663421f5ddbc837636b733fcad463de0bd
Kerry <k.of.blades@gmail.com>
2019-08-14 14:44:58 +1200

    Created json serialisation component

---
+++ DinoProject/Assets/Scripts/GeneralSystems/SerialisationComponent.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public class SerialisationComponent : MonoBehaviour
+{
+
+    // Deserialise the parent on enable
+    private void OnEnable() {
+        // Search for serialised data
+        if(File.Exists(Application.persistentDataPath + PlayerPrefs.GetString("FarmosaurSave", "Default") +"_" + this.gameObject.name + ".pso")) {
+            // Create new binary formatter
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+            // Open file
+            FileStream file = File.Open(Application.persistentDataPath + PlayerPrefs.GetString("FarmosaurSave", "Default") + "_" + this.gameObject.name + ".pso", FileMode.Open);
+            // Deserialise from JSON
+            JsonUtility.FromJsonOverwrite((string)binaryFormatter.Deserialize(file), this.gameObject);
+            // Close file
+            file.Close();
+        }
+    }
+
+
+    // Save the object when disabled
+    private void OnDisable() {
+        // Create a binary formatter
+        BinaryFormatter binaryFormatter = new BinaryFormatter();
+        // Create a filestream using the save path for this item
+        FileStream file = File.Create(Application.persistentDataPath + PlayerPrefs.GetString("FarmosaurSave", "Default") + "_" + this.gameObject.name + ".pso");
+        // Serialise object
+        binaryFormatter.Serialize(file, JsonUtility.ToJson(this.gameObject));
+
+        // Remember to perform any deregistration as required for the attached object
+    }
+
+}
